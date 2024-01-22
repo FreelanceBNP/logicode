29bcacbd5649fb67ba5e10e57d70e389e11e5821
wijarn ch <wijarn.coding@gmail.com>
2024-01-22 17:04:02 +0700

    Delete unnecessary image files and binaries

---
+++ LGC.BNP.MIKUNI/LGC.BNP.MIKUNI.Web/Controllers/StockController.cs
@@ -0,0 +1,37 @@
+﻿using LGC.BNP.MIKUNI.Web.Models;
+using LGC.BNP.MIKUNI.Web.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace LGC.BNP.MIKUNI.Web.Controllers
+{
+	public class StockController : BaseController
+	{
+		private AccountService _account;
+		private IConfiguration _config;
+		private readonly UtilityService _utility;
+		private readonly MasterService _master;
+		private readonly MailService _mail;
+		public StockController(MailService mail, IConfiguration config, AccountService account, UtilityService util, MasterService master) : base(account, util)
+		{
+			_config = config;
+			_account = account;
+			_utility = util;
+			_master = master;
+			_mail = mail;
+		}
+		public IActionResult Overview()
+		{
+			ViewBag.title = "Stock Overview";
+				var model = _account.GetList().Result;
+			foreach (var item in model)
+			{
+				item.id_encrypt = _utility.EncryptString(item.id.ToString()).ToString();
+			}
+			return View(model);
+		}
+	}
+}
--- LGC.BNP.MIKUNI/LGC.BNP.MIKUNI.WebSocket/obj/Debug/net7.0/LGC.BNP.MIKUNI.WebSocket.AssemblyInfo.cs
+++ LGC.BNP.MIKUNI/LGC.BNP.MIKUNI.WebSocket/obj/Debug/net7.0/LGC.BNP.MIKUNI.WebSocket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LGC.BNP.MIKUNI.WebSocket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a167dbfd7b2990a38a3485a5a43352ea7897fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LGC.BNP.MIKUNI.WebSocket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LGC.BNP.MIKUNI.WebSocket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
